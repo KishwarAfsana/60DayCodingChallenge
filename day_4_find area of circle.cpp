960baca92d8ebc00cbe277c3c0278a966e41a132
KishwarAfsana <119694999+KishwarAfsana@users.noreply.github.com>
2022-12-05 05:23:13 -0800

    Add files via upload

---
+++ day_4_find area of circle.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+   float pie = 3.14;
+   int radius = 6;
+   printf("\nThe radius of circle is %d",radius);
+   float area = (float)(pie* radius * radius);
+   printf("\nThe area of the given circle is %f", area);
+   return 0;
+}
