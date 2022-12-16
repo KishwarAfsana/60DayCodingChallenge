aa111abb951ed8228700f013dd08dd6be3de0059
KishwarAfsana <119694999+KishwarAfsana@users.noreply.github.com>
2022-12-16 05:35:30 -0800

    Add files via upload

---
+++ Day_14_incr_decrement operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 12, b = 50;
+    float c = 12.5,d = 50.5;
+	printf("++a = %d \n", ++a);
+    printf("--b = %d \n", --b);
+    printf("++c = %f \n", ++c);
+    printf("--d = %f \n", --d);
+    return 0;
+}
