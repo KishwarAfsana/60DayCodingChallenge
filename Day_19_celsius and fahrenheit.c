1e479c4cf68b78680f64958937dd6c60cddec627
KishwarAfsana <119694999+KishwarAfsana@users.noreply.github.com>
2022-12-22 23:21:32 +0530

    Create Day_19_celsius and fahrenheit.c

---
+++ Day_19_celsius and fahrenheit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n kishwar \n");
+    
+    float celsius, fahrenheit;
+    printf("\n\nEnter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    
+    fahrenheit = (1.8*celsius) + 32;
+    
+    printf("\n\n\nTemperature in Fahrenheit is: %f ", fahrenheit);
+    printf("\n MANUU \n");
+    return 0;
+}
